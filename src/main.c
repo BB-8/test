b52ca8ef8947f0020ee268a14de82e817b48a69a
Dimitri Gasser <dimitri.gasser@bit.admin.ch>
2015-12-07 07:19:18 +0000

    Fixes issue #1

--- src/main.c
+++ src/main.c
@@ -1,8 +1,12 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#define VERSION_MAJOR 0
+#define VERSION_MINOR 1
+
 int main(int argc, char *argv[])
 {
+    printf("Demo Project version %d.%d", VERSION_MAJOR, VERSION_MINOR);
 	printf("https://gitlab.com/gdb/demo");
 	return 0;
 }
