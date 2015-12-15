7025e77478b761d8d7672f9c49127d92ab371f62
Dimitri Gasser <dimitri.gasser@bit.admin.ch>
2015-12-15 10:32:27 +0100

    The application can now perform some math operations

--- src/main.c
+++ src/main.c
@@ -6,7 +6,23 @@
 
 int main(int argc, char *argv[])
 {
-    printf("Demo Project version %d.%d", VERSION_MAJOR, VERSION_MINOR);
-	printf("https://gitlab.com/gdb/demo");
+	int a;
+	int b;
+	int r;
+
+    printf("+ Demo Project version : %d.%d\n", VERSION_MAJOR, VERSION_MINOR);
+	printf("+ Project Home         : https://gitlab.com/gdb/demoi\n");
+
+	 a = strtol(argv[1], 0, 10 );
+	b = strtol(argv[2], 0, 10);
+		
+	if (argv[3][0] == '+') {
+		r = a+b;
+	} else if( argv[3][0] == '-') { r = a-b;}
+ 	else if (argv[3][0] == '*') { r = a * b; }
+        else if (argv[3][0] == '/') { r = a /b; }
+
+	printf ("%d\n", r);
+
 	return 0;
 }
